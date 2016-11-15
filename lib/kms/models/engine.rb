53adea107ecb9bff0cb95ef023656ee8d9b7e181
GarPit <garik.piton@gmail.com>
2016-11-15 17:13:25 +0300

    fix constant loading in production + change drops folder placement

--- lib/kms/models/engine.rb
+++ lib/kms/models/engine.rb
@@ -4,7 +4,7 @@ module Kms
     class Engine < ::Rails::Engine
       engine_name 'kms_models'
       isolate_namespace Kms::Models
-      config.autoload_paths += Dir["#{config.root}/lib/**/"]
+      config.eager_load_paths += Dir["#{config.root}/lib/**/"]
       config.to_prepare do
         Dir.glob(File.join(File.dirname(__FILE__), "../../../app/**/*_decorator*.rb")) do |c|
           require_dependency(c)
