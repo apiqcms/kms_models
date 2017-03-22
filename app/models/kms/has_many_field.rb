42563abb034d3e7ddad0bb43604041c39e7fa2b5
GarPit <garik.piton@gmail.com>
2017-03-22 16:48:07 +0300

    preserve order of has_many field values - closes #9

--- app/models/kms/has_many_field.rb
+++ app/models/kms/has_many_field.rb
@@ -3,6 +3,13 @@ module Kms
     def get_value(entry)
       entry_ids = entry.values[liquor_name]
       association_records = Kms::Entry.where(id: entry_ids)
+      if entry_ids.present?
+        # this one allows ORDER BY the IN value list like this example:
+        # SELECT * FROM "comments" WHERE ("comments"."id" IN (1,3,2,4))
+        # ORDER BY  id=1 DESC, id=3 DESC, id=2 DESC, id=4 DESC
+        order_sql = entry_ids.map {|entry_id| "id=#{entry_id} DESC"}
+        association_records = association_records.order(order_sql.join(','))
+      end
       Liquor::DropDelegation.wrap_scope(association_records)
     end
   end
