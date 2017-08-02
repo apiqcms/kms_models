32a55460325220fead1bd42b724e51c7ac56b55c
GarPit <garik.piton@gmail.com>
2017-08-02 17:40:59 +0300

    fetch item only in scope of model - page's templatable type

--- app/models/kms/page_decorator.rb
+++ app/models/kms/page_decorator.rb
@@ -1,10 +1,12 @@
 Kms::Page.class_eval do
   # fetch item by slug
-  def fetch_item!(slug)
+  def fetch_item(slug)
     return nil unless templatable?
     templatable_type.constantize.find_by_slug!(slug)
   rescue NameError
-    Kms::Entry.find_by_slug!(slug)
+    model = Kms::Model.find(templatable_type.to_i)
+    model.entries.find_by_slug(slug)
+    # Kms::Entry.find_by_slug(slug)
   end
 
   # fetch items for templatable page
